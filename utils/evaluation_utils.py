ae3adac769136964af349b1c41631e16a0bbb2aa
Khoi Duc Nguyen <47294247+khoiucd@users.noreply.github.com>
2024-03-17 18:07:53 -0500

    Create evaluation_utils.py
    Computing confident interval.

---
+++ utils/evaluation_utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def compute_confidence_interval(data):
+    """
+    Compute 95% confidence interval
+    :param data: An array of mean accuracy (or mAP) across a number of sampled episodes.
+    :return: the 95% confidence interval for this data.
+    """
+    a = 1.0 * np.array(data)
+    m = np.mean(a)
+    std = np.std(a)
+    pm = 1.96 * (std / np.sqrt(len(a)))
+    return m, pm
